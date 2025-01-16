fa63838ee26e1ecaef85f90eb1ca09c959677d4c
Andy Farmer <andy.farmer@travelopia.com>
2025-01-16 20:43:57 +0000

    SETUP - #16 Setup Code Formating and Linting

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,17 @@
 const config = {
     images: {
       remotePatterns: [
-        { hostname: 'cdn.sanity.io' },
-        { hostname: 'source.unsplash.com' },
+        { hostname: "cdn.sanity.io" },
+        { hostname: "source.unsplash.com" },
       ],
     },
     typescript: {
       // Set this to false if you want production builds to abort if there's type errors
-      ignoreBuildErrors: process.env.VERCEL_ENV === 'production',
+      ignoreBuildErrors: process.env.VERCEL_ENV === "production",
     },
     eslint: {
       /// Set this to false if you want production builds to abort if there's lint errors
-      ignoreDuringBuilds: process.env.VERCEL_ENV === 'production',
+      ignoreDuringBuilds: process.env.VERCEL_ENV === "production",
     },
     logging: {
       fetches: {
@@ -21,12 +21,12 @@ const config = {
     },
     env: {
       // Matches the behavior of `sanity dev` which sets styled-components to use the fastest way of inserting CSS rules in both dev and production. It's default behavior is to disable it in dev mode.
-      SC_DISABLE_SPEEDY: 'false',
+      SC_DISABLE_SPEEDY: "false",
     },
     experimental: {
       taint: true,
+			typedRoutes: true,
     },
-  }
-  
-  export default config
-  
+  };
+
+  export default config;
