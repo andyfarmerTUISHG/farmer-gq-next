5841603ed1dcd42329c9bd54cca601598751fd9e
Andy Farmer <andy.farmer@travelopia.com>
2025-01-21 20:56:19 +0000

    refactor(sanity): refactor config for Sanity

--- schema/schema.js
+++ schema/schema.js
@@ -1,12 +1,12 @@
 // Then we give our schema to the builder and provide the result to Sanity
-import article from './articles';
-import tags from './tags';
-import person from './person';
-import profile from './profile';
+import article from "./articles";
+import person from "./person";
+import profile from "./profile";
+import tags from "./tags";
 
 export default [
   article,
   tags,
   person,
   profile
-]
+];
