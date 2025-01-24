0e56358a58f2bce43c49ea502338c50c45e52ada
Andy Farmer <andy.farmer@travelopia.com>
2025-01-24 16:58:02 +0000

    feat(article): refactored Article listing page to use load-query methods, and removed sanity details

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,5 +1,28 @@
 import { groq } from "next-sanity";
 
+
+export const paginatedArticlesQuery = groq`
+  *[_type == "article"] | order(_id) [$skip...$pageSize] {
+    _id,
+    "slug": slug.current,
+    name,
+    createdDate,
+    bodycopy,
+    author,
+    "articleCount": count(*[_type == "article"])
+  }
+`;
+export const allArticlesQuery = groq`
+  *[_type == "article"] {
+    _id,
+    "slug": slug.current,
+    name,
+    createdDate,
+    bodycopy,
+    author,
+  }
+`;
+
 export const articleBySlugQuery = groq`
     *[_type == "article" && slug.current == $slug][0] {
         _id,
@@ -7,18 +30,16 @@ export const articleBySlugQuery = groq`
         name,
         createdDate,
         bodycopy,
-		author,
+		    author,
     }
 `;
 
 export const settingsQuery = groq`
   *[_type == "settings"][0]{
-
     menuItems[]->{
       _type,
       "slug": slug.current,
       title
     },
-
   }
 `;
