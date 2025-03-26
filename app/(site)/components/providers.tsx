a957341f0183891acaddd5164ffb56a3306d2081
Andy Farmer <andy.farmer@travelopia.com>
2025-03-26 16:31:52 +0000

    chore(project): upgrade to ESLint9

--- app/(site)/components/providers.tsx
+++ app/(site)/components/providers.tsx
@@ -1,22 +1,10 @@
-
-
 import { HeroUIProvider } from "@heroui/react";
 import type { ReactNode } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
-
-
   return (
-
-      <HeroUIProvider
-
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        className="flex h-full w-full flex-col"
-      >
-
-					{children}
-
-      </HeroUIProvider>
-
+    <HeroUIProvider className="flex h-full w-full flex-col">
+      {children}
+    </HeroUIProvider>
   );
 }
