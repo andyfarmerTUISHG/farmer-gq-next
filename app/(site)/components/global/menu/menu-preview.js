fb1277f1f823efd2c52237be8c7a96970ac00ce8
Andy Farmer <akf0676@sky.com>
2025-04-15 19:58:09 +0100

    fix(menu): revert TS changes due to build issues

--- app/(site)/components/global/menu/menu-preview.js
+++ app/(site)/components/global/menu/menu-preview.js
@@ -3,19 +3,7 @@ import { useQuery } from "@/sanity/loader/use-query";
 
 import MenuLayout from "./menu-layout";
 
-interface MenuData {
-  menuItems?: Array<{
-    _type: string;
-    slug?: string;
-    name: string;
-  }>;
-}
-
-interface MenuPreviewProps {
-  initial: MenuData;
-}
-
-export default function MenuPreview({ initial }: MenuPreviewProps) {
+export default function MenuPreview({ initial }) {
   const { data, encodeDataAttribute } = useQuery(
     settingsQuery,
     {},
