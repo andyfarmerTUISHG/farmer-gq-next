8ba7a9ada575f22eaf059b0c4403af5cbbf4349d
Andy Farmer <akf0676@sky.com>
2025-07-20 15:14:14 +0100

    refactor/sanity live edit updates (#51)
    * chore(project): update dependencies
    
    * feat(project): generation of types and schema
    
    * chore(project): update workspace files
    
    * feat(project): update sanity config and live functions
    
    * feat(project): live edit updayer

--- sanity/loader/live-visual-editing.tsx
+++ sanity/loader/live-visual-editing.tsx
@@ -1,15 +1,31 @@
 "use client";
 
+import type { SanityClient } from "@sanity/client";
 import { useLiveMode } from "@sanity/react-loader";
 import { VisualEditing } from "next-sanity";
 
 import { client } from "@/sanity/lib/client";
 
 // Always enable stega in Live Mode
-const stegaClient = client.withConfig({ stega: true });
+const stegaClient = client.withConfig({
+  stega: {
+    enabled: true,
+    studioUrl:
+      process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+        ? "https://your-production-studio-url.com"
+        : "http://localhost:3000",
+  },
+}) as unknown as SanityClient;
 
 export default function LiveVisualEditing() {
-  useLiveMode({ client: stegaClient });
+  useLiveMode({
+    client: stegaClient,
+    // Add these options to prevent potential issues
+    allowStudioOrigin:
+      process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+        ? "https://your-production-studio-url.com"
+        : "http://localhost:3000",
+  });
 
   return <VisualEditing />;
 }
