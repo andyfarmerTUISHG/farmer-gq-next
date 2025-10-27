3ed249fd37def86e2174d9f582e920522a99f41d
Andy Farmer <akf0676@sky.com>
2025-10-27 21:26:44 +0000

    fix(project): update ESLint Settings as next build no longer runs linting
    - As part of upgrade ran codemod for the eslint settings see https://nextjs.org/blog/next-16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,66 +1,57 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import checkFile from "eslint-plugin-check-file";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
 
-export default defineConfig([
-  {
-    ignores: [
-      ".next/**",
-      "dist/**",
-      "node_modules/**",
-      "public/studio/**",
-      "*.d.ts",
-      // TOOO: Remove this when we have a better way to handle this
-      "sanity.config.ts",
-      "sanity.types.ts",
-      "sanity/loader/load-query.ts"
+export default defineConfig([{
+  ignores: [
+    ".next/**",
+    "dist/**",
+    "node_modules/**",
+    "public/studio/**",
+    "*.d.ts",
+    // TOOO: Remove this when we have a better way to handle this
+    "sanity.config.ts",
+    "sanity.types.ts",
+    "sanity/loader/load-query.ts"
+  ],
+}, ...nextCoreWebVitals, ...nextTypescript, {
+  plugins: {
+    "simple-import-sort": simpleImportSort,
+    "check-file": checkFile,
+  },
+
+  rules: {
+    "prefer-arrow-callback": ["error"],
+    "prefer-template": ["error"],
+    semi: ["error"],
+    quotes: ["error", "double"],
+    "simple-import-sort/imports": "warn",
+    "simple-import-sort/exports": "warn",
+    "react-hooks/exhaustive-deps": "error",
+
+    "check-file/filename-naming-convention": [
+      "error",
+      {
+        "**/*.{js,jsx,ts,tsx}": "KEBAB_CASE",
+      },
+      {
+        ignoreMiddleExtensions: true,
+      },
+    ],
+
+    "check-file/folder-naming-convention": [
+      "error",
+      {
+        "**/!^[.*": "KEBAB_CASE",
+      },
     ],
   },
-  ...compat.extends("next/core-web-vitals", "next/typescript" ),
-  {
-    plugins: {
-      "simple-import-sort": simpleImportSort,
-      "check-file": checkFile,
-    },
-
-    rules: {
-      "prefer-arrow-callback": ["error"],
-      "prefer-template": ["error"],
-      semi: ["error"],
-      quotes: ["error", "double"],
-      "simple-import-sort/imports": "warn",
-      "simple-import-sort/exports": "warn",
-      "react-hooks/exhaustive-deps": "error",
-
-      "check-file/filename-naming-convention": [
-        "error",
-        {
-          "**/*.{js,jsx,ts,tsx}": "KEBAB_CASE",
-        },
-        {
-          ignoreMiddleExtensions: true,
-        },
-      ],
-
-      "check-file/folder-naming-convention": [
-        "error",
-        {
-          "**/!^[.*": "KEBAB_CASE",
-        },
-      ],
-    },
-  },
-]);
+}]);
