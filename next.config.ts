64e330c6fc6bfe79c953512c7e2e8d98c50269bc
Andy Farmer <akf0676@sky.com>
2025-10-27 21:52:13 +0000

    fix(project): update next config to remove eslint settings and api revalidate (#66)

--- next.config.ts
+++ next.config.ts
@@ -20,10 +20,6 @@ const config: NextConfig = {
     // Set this to false if you want production builds to abort if there's type errors
     ignoreBuildErrors: process.env.VERCEL_ENV === "production",
   },
-  eslint: {
-    /// Set this to false if you want production builds to abort if there's lint errors
-    ignoreDuringBuilds: process.env.VERCEL_ENV === "production",
-  },
   logging: {
     fetches: {
       fullUrl: true,
