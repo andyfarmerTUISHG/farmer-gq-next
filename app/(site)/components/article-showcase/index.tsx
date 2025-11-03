52e65722d4671842c92127280762326aba204827
Andy Farmer <akf0676@sky.com>
2025-11-03 19:28:12 +0000

    fix(article-showcase): fix Query for Showcase Articles (#78)

--- app/(site)/components/article-showcase/index.tsx
+++ app/(site)/components/article-showcase/index.tsx
@@ -1,12 +1,12 @@
 import { notFound } from "next/navigation";
 
 import { sanityFetch } from "@/sanity/lib/live";
-import { allArticlesQuery } from "@/sanity/lib/queries";
+import { articleShowcaseQuery } from "@/sanity/lib/queries";
 
 import ArticleShowcaseLayout from "./article-showcase-layout";
 
 export default async function ArticleShowcase() {
-  const { data } = await sanityFetch({ query: allArticlesQuery });
+  const { data } = await sanityFetch({ query: articleShowcaseQuery });
 
   if (!data) {
     notFound();
